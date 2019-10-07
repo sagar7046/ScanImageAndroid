dd3533ea164b5be1a146cd81d5204e78d9fdcd8d
OceanTech <gayasagarv@gmail.com>
2019-10-07 12:25:53 +0530

    small change

--- app/src/main/java/com/example/scanimage/MainActivity.java
+++ app/src/main/java/com/example/scanimage/MainActivity.java
@@ -55,6 +55,8 @@ public class MainActivity extends AppCompatActivity
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+
+            //shopw the commit comment
         }
     }
 
