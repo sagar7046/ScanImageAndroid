0ae657d9200ca400af84214775d015a7ac37bf78
OceanTech <gayasagarv@gmail.com>
2019-11-04 10:32:34 +0530

    implemented flirebase text recognizer

--- app/src/main/java/com/example/scanimage/MainActivity.java
+++ app/src/main/java/com/example/scanimage/MainActivity.java
@@ -15,14 +15,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
--- app/src/main/java/com/example/scanimage/ScanActivity.java
+++ app/src/main/java/com/example/scanimage/ScanActivity.java
@@ -1,14 +1,80 @@
 package com.example.scanimage;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.net.URI;
 
 public class ScanActivity extends AppCompatActivity {
-
+    Button btn_scan_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        btn_scan_text=findViewById(R.id.btn_scan_text);
+        btn_scan_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,100);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK)
+        {
+            Uri selectedImage=data.getData();
+            FirebaseVisionImage firebaseVisionImage;
+            try {
+
+
+                firebaseVisionImage=FirebaseVisionImage.fromFilePath(getApplicationContext(), selectedImage);
+                FirebaseVisionTextRecognizer detector= FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+                Task<FirebaseVisionText> result =
+                        detector.processImage(firebaseVisionImage)
+                                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                                    @Override
+                                    public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                                        // Task completed successfully
+                                        // ...
+                                        for (FirebaseVisionText.TextBlock block : firebaseVisionText.getTextBlocks()) {
+                                            Rect boundingBox = block.getBoundingBox();
+                                            Point[] cornerPoints = block.getCornerPoints();
+                                            String text = block.getText();
+                                            Log.d("VISION",text);
+                                        }
+                                    }});
+            }
+            catch(Exception ex)
+            {
+                System.out.println("Error in reading" +ex.getMessage());
+            }
+
+            Log.d("PATH",selectedImage.getPath());
+        }
     }
 }
